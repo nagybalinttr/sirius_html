b901ecbe70262e07f76b6f08002d01944070e317
Nagy Bálint [intermedius] <nagy.balint.tr@gmail.com>
2017-05-26 11:29:07 +0200

    added missing files

---
+++ gyakorlo2/tti.js
@@ -0,0 +1,12 @@
+function tti() {
+  var suly = document.getElementById('suly').value;
+  var magassag = document.getElementById('magassag').value;
+  if( (suly != '' && magassag != '') && (suly > 0 && magassag > 0) ){
+    suly = parseFloat(suly);
+    magassag = parseFloat(magassag);
+    var tti = (suly / magassag) * magassag * 0.0001;
+    alert('Index: ' + tti);
+  } else {
+    alert('Hibas adatbevitel!');
+  }
+}
